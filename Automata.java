a0de5bcc3d5e56dac3677ef24954f81c456a56d0
Amayrami20 <148155357+Amayrami20@users.noreply.github.com>
2024-10-03 13:16:00 -0600

    AutomataPractica

---
+++ Automata.java
@@ -0,0 +1,14 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Automata {
+        Scanner lector= new Scanner (System.in);
+        public String sId="";
+        public boolean bEstado=false;
+        public ArrayList <String> Ruta=null;
+        public ArrayList <Automata> hijos=null;
+    }
+    
+
